32d74eae63c636362f66a910af7e768c804b5483
PSH <PSH@192.168.219.100>
2021-11-01 16:47:43 +0900

    박성환 11/1 : 게시판 이미지 관련 업데이트

---
+++ src/main/java/com/kh/project/board/vo/FreeImgVO.java
@@ -0,0 +1,42 @@
+package com.kh.project.board.vo;
+
+public class FreeImgVO {
+
+	private String freeImgCode;
+	private String freeImgOrignName;
+	private String freeImgAttachedName;
+	private int boardNumFree;
+
+	public String getFreeImgCode() {
+		return freeImgCode;
+	}
+
+	public void setFreeImgCode(String freeImgCode) {
+		this.freeImgCode = freeImgCode;
+	}
+
+	public String getFreeImgOrignName() {
+		return freeImgOrignName;
+	}
+
+	public void setFreeImgOrignName(String freeImgOrignName) {
+		this.freeImgOrignName = freeImgOrignName;
+	}
+
+	public String getFreeImgAttachedName() {
+		return freeImgAttachedName;
+	}
+
+	public void setFreeImgAttachedName(String freeImgAttachedName) {
+		this.freeImgAttachedName = freeImgAttachedName;
+	}
+
+	public int getBoardNumFree() {
+		return boardNumFree;
+	}
+
+	public void setBoardNumFree(int boardNumFree) {
+		this.boardNumFree = boardNumFree;
+	}
+
+}
---
+++ src/main/java/com/kh/project/board/vo/NoticeImgVO.java
@@ -0,0 +1,42 @@
+package com.kh.project.board.vo;
+
+public class NoticeImgVO {
+
+	private String noticeImgCode;
+	private String noticeImgOrignName;
+	private String noticeImgAttachedName;
+	private int boardNumNotice;
+
+	public String getNoticeImgCode() {
+		return noticeImgCode;
+	}
+
+	public void setNoticeImgCode(String noticeImgCode) {
+		this.noticeImgCode = noticeImgCode;
+	}
+
+	public String getNoticeImgOrignName() {
+		return noticeImgOrignName;
+	}
+
+	public void setNoticeImgOrignName(String noticeImgOrignName) {
+		this.noticeImgOrignName = noticeImgOrignName;
+	}
+
+	public String getNoticeImgAttachedName() {
+		return noticeImgAttachedName;
+	}
+
+	public void setNoticeImgAttachedName(String noticeImgAttachedName) {
+		this.noticeImgAttachedName = noticeImgAttachedName;
+	}
+
+	public int getBoardNumNotice() {
+		return boardNumNotice;
+	}
+
+	public void setBoardNumNotice(int boardNumNotice) {
+		this.boardNumNotice = boardNumNotice;
+	}
+
+}
